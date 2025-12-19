8b337bdb3613b9db94b336623fc0ddae83a575c6
root <root@ip-172-31-23-167.ec2.internal>
2025-12-19 07:36:17 +0000

    now iam adding data

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ sum = num1 + num2
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 Dev-2 some data added 1 line of data
+
+without pull dev1 is going to add code
+
+Dev1 add some  code 
+
